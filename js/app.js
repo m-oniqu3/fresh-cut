a4e5cab558753889e1c983c3edc60b7f050d6ab4
Monique McIntyre <skaiwilliams85@gmail.com>
2022-06-12 16:23:33 -0500

    Refactored the getPictureInfo function

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ menuIcon.addEventListener("click", () => {
 // array of team info
 const teamInfo = [
   {
+    id: "donny",
     name: "Donny Pancho",
     image:
       "https://images.unsplash.com/photo-1582893561942-d61adcb2e534?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=391&q=80",
@@ -31,6 +32,7 @@ const teamInfo = [
       "Looking for a high-end barbering experience? Look no further than Donny. With years of experience he gives his clients the perfect cut, every time. He's also a master of color, so if you're looking to add a little something extra to your look, Don is your man. Book your appointment with Donny today and you'll be sure to walk out of the salon looking and feeling your best.",
   },
   {
+    id: "toris",
     name: "Toris Dorelli",
     image:
       "https://images.unsplash.com/photo-1571154182577-a63c5101f8d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
@@ -38,6 +40,7 @@ const teamInfo = [
       "Toris is one of the best.  10 years in the business and he has mastered the art of cutting hair. He is always up-to-date on the latest trends and knows how to style any hair type. Toris is also a great listener and takes the time to understand each client's individual needs. He always strives to provide the best possible service and leaves every client looking and feeling their best. ",
   },
   {
+    id: "luca",
     name: "Luca Bianchi",
     image:
       "https://images.unsplash.com/photo-1635273051937-a0ddef9573b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
@@ -45,6 +48,7 @@ const teamInfo = [
       "want a luxurious haircut experience? then you need to book with Luca.  His precision and attention to detail is unmatched, and he always makes sure to listen to his clients to ensure they get the cut they really want. Trust us, Luca is the one of best in the business and you won't be disappointed!",
   },
   {
+    id: "marcus",
     name: "Marcus Bryant",
     image:
       "https://images.unsplash.com/photo-1567894340315-735d7c361db0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=437&q=80",
@@ -60,21 +64,11 @@ teamPictures.forEach((picture) => {
 
 //get the info of the team member clicked on and display it in the dom
 const getPictureInfo = (id) => {
-  if (id === "donny") {
-    teamMemberPicture[0].setAttribute("src", teamInfo[0].image);
-    barberName.innerHTML = teamInfo[0].name;
-    description.innerHTML = teamInfo[0].description;
-  } else if (id === "toris") {
-    teamMemberPicture[0].setAttribute("src", teamInfo[1].image);
-    barberName.innerHTML = teamInfo[1].name;
-    description.innerHTML = teamInfo[1].description;
-  } else if (id === "luca") {
-    teamMemberPicture[0].setAttribute("src", teamInfo[2].image);
-    barberName.innerHTML = teamInfo[2].name;
-    description.innerHTML = teamInfo[2].description;
-  } else if (id === "marcus") {
-    teamMemberPicture[0].setAttribute("src", teamInfo[3].image);
-    barberName.innerHTML = teamInfo[3].name;
-    description.innerHTML = teamInfo[3].description;
-  }
+  teamInfo.filter((member) => {
+    if (member.id === id) {
+      teamMemberPicture[0].setAttribute("src", member.image);
+      barberName.innerHTML = member.name;
+      description.innerHTML = member.description;
+    }
+  });
 };
